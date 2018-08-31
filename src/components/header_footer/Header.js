884068df2886c5099e813e014772a702b2bd3c18
jesslalanne <jessica.lalanne@gmail.com>
2018-08-31 14:54:50 +0200

    Add Header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { AppBar, Toolbar, Button } from "@material-ui/core";
+
+import  { Link } from "react-router-dom";
+
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                HEADER
-            </div>
+            <AppBar
+                position="fixed"
+                style={{
+                    backgroundColor:"#98C5E9",
+                    boxShadow: "none",
+                    padding: "10 px 0",
+                    borderBottom: "2px solid #00285E"
+                }}
+            >
+                <Toolbar styles={{display: "flex"}}>
+                    <div styles={{FlexGrow: 1}}>
+                        <div className="header_logo">LOGO</div>
+                    </div>
+                    <Link to="/the_team">
+                        <Button color="inherit">The Team</Button>
+                    </Link>
+
+                    <Link to="/the_match">
+                    <Button color="inherit">Matches</Button>
+                    </Link>
+
+                </Toolbar>
+
+            </AppBar>
         );
     }
 }
