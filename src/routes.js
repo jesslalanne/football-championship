91301c740eaa877e50695a539e31a9e9c2299120
jesslalanne <jessica.lalanne@gmail.com>
2018-08-31 14:36:33 +0200

    Add Layout

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "./hoc/Layout";
+
+const Routes = (props) => {
+  return (
+   <Layout>
+     Second Line
+   </Layout>
+  )
+}
+
+export default Routes;
