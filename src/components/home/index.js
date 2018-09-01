5f589cd6c1d8453012dce848bd73a9cd690f30a8
jesslalanne <jessica.lalanne@gmail.com>
2018-09-01 15:07:03 +0200

    Prettier

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Featured from './featured';
+import React from "react";
+import Featured from "./featured";
 
 const Home = () => {
-    return (
-        <div className="bck_blue">
-            <Featured/>
-        </div>
-    );
+  return (
+    <div className="bck_blue">
+      <Featured />
+    </div>
+  );
 };
 
 export default Home;
